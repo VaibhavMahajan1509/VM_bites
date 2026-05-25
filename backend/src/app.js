03556aa366baeb4595817b9d1cd83d2e6d05f85c
Vaibhav <vaibhav.mahajan1509@email.comgit config -listgit config --global user.email vaibhav.mahajan1509@email.com>
2026-05-25 20:34:18 +0530

    cors setup for frontend vit url

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend.vercel.app",
+      "https://vm-bites.vercel.app",
     ],
     credentials: true,
   })
