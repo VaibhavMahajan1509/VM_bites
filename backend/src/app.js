6c7887b6b65c61b97d4b47f2752ecfcc55e0284a
Vaibhav <vaibhav.mahajan1509@email.comgit config -listgit config --global user.email vaibhav.mahajan1509@email.com>
2026-05-25 17:44:30 +0530

    cors setup done

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,23 +7,29 @@ import foodRoutes from "./routes/foodRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
-
 import paymentRouter from "./routes/paymentRoute.js";
 
 const app = express();
 
-// CORS 
+// CORS
 app.use(
   cors({
     origin: [
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "https://your-frontend.vercel.app",
     ],
     credentials: true,
   })
 );
 
+// MIDDLEWARES
 app.use(express.json());
 app.use(cookieParser());
+
+// HEALTH ROUTE
+app.get("/", (req, res) => {
+  res.send("API Running Successfully");
+});
 
 // ROUTES
 app.use("/api/auth", authRoutes);
@@ -31,7 +37,13 @@ app.use("/api/foods", foodRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/order", orderRoutes);
 app.use("/api/admin", adminRoutes);
-
 app.use("/api/payment", paymentRouter);
+
+// 404 HANDLER
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route Not Found",
+  });
+});
 
 export default app;
